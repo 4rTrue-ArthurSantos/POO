0cc9751954b157700e85cd14ca5b6286a32093c8
Arthur Santos <arthur.pentester@gmail.com>
2023-08-31 17:00:08 -0300

    fifth commit

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-class Usuario:
-    cargo = 'usuario'
+class Pessoa:
 
-    def __init__(self,nome,idade,altura):
+    def __init__(self,nome,cpf,altura):
         self.nome = nome
-        self.idade = idade
+        self.cpf = cpf
         self.altura = altura
 
-    def retorna_altura(self):
-        print(self.altura)
+    def exibe_cpf(self):
+        return self.cpf
 
-    @classmethod
-    def cargo_usuario(cls):
-        cls.cargo = "Gerente"
-        print(cls.cargo)
+class Secretaria(Pessoa):
+    def __init__(self,nome,cpf,altura,id_secretaria):
+        super().__init__(nome,cpf,altura)
+        self.id_secretaria = id_secretaria 
 
-    @staticmethod
-    def e_verme(verme):
-        if verme == 'fiter':
-            return True 
+class Vendedor(Pessoa):
+    def __init__(self,nome,cpf,altura,vendas):
+        super().__init__(nome,cpf,altura)
+        self.vendas = vendas 
 
 
-usuario1 = Usuario('arthur',22,1.72)
+sec1 = Secretaria('Maria','124355','170','2')
+print(sec1.exibe_cpf())
