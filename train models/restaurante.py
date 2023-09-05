f7224dda9bb0cec44a514f09d577b27567c061d9
Arthur Santos <arthur.pentester@gmail.com>
2023-09-04 23:57:52 -0300

    14th commit

--- train models/restaurante.py
+++ train models/restaurante.py
@@ -1,9 +1,20 @@
 class Funcionario:
     def __init__(self,nome,idade,salario):
         self.__nome = nome
-        self.__idade = idade
-        self.__salario = salario
+        self.idade = idade
+        self.salario = salario
 
     @property
     def nome(self):
         return self.__nome
+    
+    @property
+    def salario(self):
+        return self.salario
+
+    @salario.setter
+    def salario(self,valor):
+        if isinstance(valor,str) and 'R$' in valor:
+            valor = valor.split('R$','')
+        self.salario = float(valor)
+        
