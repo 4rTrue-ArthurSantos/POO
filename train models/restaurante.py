619d92aa7e70d308f41f837eb37834ef9edf8dde
Arthur Santos <arthur.pentester@gmail.com>
2023-09-05 00:07:36 -0300

    bug fix

--- train models/restaurante.py
+++ train models/restaurante.py
@@ -6,19 +6,19 @@ class Funcionario:
 
     @property
     def nome(self):
-        return self.__nome
+        return self.__nome.title()
     
     @property
     def salario(self):
-        return self.salario
+        return self._salario
 
     @salario.setter
     def salario(self,valor):
         if isinstance(valor,str) and 'R$' in valor:
-            valor = valor.split('R$','')
-        self.salario = float(valor)
+            valor = valor.replace('R$','')
+        self._salario = float(valor)
 
 
 if __name__ == '__main__':
-    funcionario = Funcionario('Jorge',27,'R$2500')
+    funcionario = Funcionario('jorge',27,'R$2500')
     print(funcionario.salario)  
