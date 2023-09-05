96bb276df584d7fedfe3f9dcb45349ba4955e9b6
Arthur Santos <arthur.pentester@gmail.com>
2023-09-05 01:31:44 -0300

    implementação da classe 'Garcon'

--- train models/restaurante.py
+++ train models/restaurante.py
@@ -31,6 +31,17 @@ class Cozinheiro(Funcionario):
         else:
             print(f'{prato} não está disponível no nosso cardápio')
 
+class Garcon(Funcionario):
+
+    def __init__(self, nome, idade, salario):
+        super().__init__(nome, idade, salario)
+
+    def servir(self):
+        print(f'{self.nome} já está trazendo o seu pedido!')
+    
+    def recebe_gorgeta(self,gorgeta):
+        print(f'Nosso funcionário {self.nome} agradece sua gorgeta no valor de R${gorgeta}!')
+
 
 
 
@@ -38,3 +49,10 @@ if __name__ == '__main__':
     jorge = Cozinheiro('jorge',27,'R$2500')
     print(jorge.salario)
     jorge.cozinhar('arroz')
+    print()
+
+    pedro = Garcon('pedro',19,'1550')
+    print(pedro.salario)
+    pedro.servir()
+    pedro.recebe_gorgeta(10)
+    print()
