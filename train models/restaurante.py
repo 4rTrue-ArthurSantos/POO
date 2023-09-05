d3f3b018ee134b9b9f58ffdba8ccf58d94944f4b
Arthur Santos <arthur.pentester@gmail.com>
2023-09-05 01:26:11 -0300

    adição da classe 'Cozinheiro'

--- train models/restaurante.py
+++ train models/restaurante.py
@@ -18,7 +18,23 @@ class Funcionario:
             valor = valor.replace('R$','')
         self._salario = float(valor)
 
+class Cozinheiro(Funcionario):
+    cardapio = ['salada','arroz','sopa','torta']
+
+    def __init__(self, nome, idade, salario):
+        super().__init__(nome, idade, salario)
+    
+    @classmethod
+    def cozinhar(cls,prato):
+        if prato in cls.cardapio:
+            print(f'O cozinheiro já está preparando {prato}')
+        else:
+            print(f'{prato} não está disponível no nosso cardápio')
+
+
+
 
 if __name__ == '__main__':
-    funcionario = Funcionario('jorge',27,'R$2500')
-    print(funcionario.salario)  
+    jorge = Cozinheiro('jorge',27,'R$2500')
+    print(jorge.salario)
+    jorge.cozinhar('arroz')
